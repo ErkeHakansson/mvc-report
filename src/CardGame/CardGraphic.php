4ba950e03b85b6bb4d643ba08ce1cb0990d73d5b
Erke Hakansson <erhk22@student.bth.se>
2023-05-20 16:00:56 +0200

    Added new class to card game

---
+++ src/CardGame/CardGraphic.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\CardGame;
+
+// Arv
+
+class CardGraphic extends Card
+    // private variabel går inte att se in subclass
+    // man kan hämta den variabel genom public method
+{
+    private array $representationGraphic = [
+        '🃑',
+        '🃒',
+        '🃓',
+        '🃔',
+        '🃕',
+        '🃖',
+        '🃗',
+        '🃘',
+        '🃙',
+        '🃚',
+        '🃛',
+        '🃝',
+        '🃞',
+        '🂡',
+        '🂢',
+        '🂣',
+        '🂤',
+        '🂥',
+        '🂦',
+        '🂧',
+        '🂨',
+        '🂩',
+        '🂪',
+        '🂫',
+        '🂭',
+        '🂮',
+        '🂱',
+        '🂲',
+        '🂳',
+        '🂴',
+        '🂵',
+        '🂶',
+        '🂷',
+        '🂸',
+        '🂹',
+        '🂺',
+        '🂻',
+        '🂽',
+        '🂾',
+        '🃁',
+        '🃂',
+        '🃃',
+        '🃄',
+        '🃅',
+        '🃆',
+        '🃇',
+        '🃈',
+        '🃉',
+        '🃊',
+        '🃋',
+        '🃍',
+        '🃎',
+    ];
+
+
+    public function __construct()
+    {
+        parent::__construct(); // funktion från class Card
+    }
+
+    // Används i class CardHand, för att hämta private array
+    public function getVariable(): array
+    {
+        return $this->representationGraphic;
+    }
+
+}
